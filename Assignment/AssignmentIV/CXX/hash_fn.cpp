75d33ae6768fa8ea1cd155b06fade6dc7a39a6b3
曾偉翔 <kkevin9563@gmail.com>
2025-11-19 01:20:00 +0800

    Implement integer and string hash functions

--- Assignment/AssignmentIV/CXX/hash_fn.cpp
+++ Assignment/AssignmentIV/CXX/hash_fn.cpp
@@ -1,1 +1,24 @@
+#include "hash_fn.hpp"
+
+int myHashInt(int key, int m) {
+    if(m <= 0) return -1;
+    int index = k % m;
+    if(index < 0) index = (index + m) % m ;
+    
+    return index; 
+}
+
+int myHashString(const std::string& str, int m) {
+  if(m <= 0) return -1;
+  if(str.empty()) return -1;
+
+  unsigned long sum = 0;
+  for(char c : str){
+    sum += static_cast<unsigned long>(c);
+  }
+
+  int index = static_cast<int>(sum % m);
+  if(index < 0)index = (index + m) % m;
+  return index;
+}
 
