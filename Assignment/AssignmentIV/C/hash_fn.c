215e3ad9fc1b61092bf9c75b296eafc5d59c5e99
曾偉翔 <kkevin9563@gmail.com>
2025-11-19 01:26:10 +0800

    Update hash_fn.c

--- Assignment/AssignmentIV/C/hash_fn.c
+++ Assignment/AssignmentIV/C/hash_fn.c
@@ -1,1 +1,24 @@
+#include "hash_fn.h"
+#include <stdio.h>
+#include <string.h>
 
+int myHashInt(int key, int m) {
+    if (m <= 0) return -1; 
+    int index = key % m;
+    if (index < 0) index = (index + m) % m; 
+    return index;
+}
+
+int myHashString(const char* str, int m) {
+    if (m <= 0) return -1; 
+    if (str == NULL || strlen(str) == 0) return -1; 
+
+    unsigned long sum = 0;
+    for (int i = 0; str[i] != '\0'; i++) {
+        sum += (unsigned long)str[i];
+    }
+
+    int index = (int)(sum % m);
+    if (index < 0) index = (index + m) % m; 
+    return index;
+}
