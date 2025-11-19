0923766430b0860be13987455b7604d43a8886b6
曾偉翔 <kkevin9563@gmail.com>
2025-11-19 23:21:23 +0800

    更新myHashInt
    因先前的方式較簡單且集中，因此更改為平方混合法，利用加、乘法混合，打亂key結構

--- Assignment/AssignmentIV/CXX/hash_fn.cpp
+++ Assignment/AssignmentIV/CXX/hash_fn.cpp
@@ -1,11 +1,8 @@
 #include "hash_fn.hpp"
 
 int myHashInt(int key, int m) {
-    if(m <= 0) return -1;
-    int index = key % m;
-    if(index < 0) index = (index + m) % m ;
-    
-    return index; 
+    unsigned int hash = key * key + key * 3 + 17;
+    return hash % m; 
 }
 
 int myHashString(const std::string& str, int m) {
