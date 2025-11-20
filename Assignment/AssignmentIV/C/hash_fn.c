88a6060dc7363d4eef2aa6709eaefab918c07063
曾偉翔 <kkevin9563@gmail.com>
2025-11-21 00:50:45 +0800

    Update hash_fn.c
    將C++版本轉成C

--- Assignment/AssignmentIV/C/hash_fn.c
+++ Assignment/AssignmentIV/C/hash_fn.c
@@ -1,23 +1,27 @@
 #include "hash_fn.h"
 
 int myHashInt(int key, int m) {
-    if (m <= 0) return -1; 
-    int index = key % m;
-    if (index < 0) index = (index + m) % m; 
-    
+    if (m <= 0) return -1;
+
+    int hash = key * 97 + 103;
+    int index = hash % m;
+    if (index < 0) index += m;
+
     return index;
 }
 
 int myHashString(const char* str, int m) {
-    if (m <= 0) return -1; 
-    if (str == NULL || strlen(str) == 0) return -1; 
+    if (m <= 0 || str == NULL || strlen(str) == 0) return -1;
 
-    unsigned long sum = 0;
+    const int a = 31;
+    const int b = 17;
+
+    unsigned long long hash = 0;
+
+    
     for (int i = 0; str[i] != '\0'; i++) {
-        sum += (unsigned long)str[i];
+        hash = (hash + a * (unsigned char)str[i] + b) % m;
     }
 
-    int index = (int)(sum % m);
-    if (index < 0) index = (index + m) % m; 
-    return index;
+    return (int)hash;
 }
